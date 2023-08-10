5f06eaa2bde029e4e81c7f814a5d7d76675b4aa7
mmanyuat <mmanyuat201@gmail.com>
2023-08-10 11:16:53 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - program to print a sentence
+ * Return: always 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
