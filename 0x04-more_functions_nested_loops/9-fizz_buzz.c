a6c8a47db3c304d924c3109abd5f4e41d99e381d
mmanyuat <mmanyuat201@gmail.com>
2023-08-18 02:58:12 -0700

    fizz_buzz project

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print numbers from 1-100
+ * Return: void
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+	if (i % 15 == 0)
+	printf("FizzBuzz");
+	else if (i % 3 == 0)
+	printf("Fizz");
+	else if (i % 5 == 0)
+	printf("Buzz");
+	else
+	printf("%i", i);
+	if (i < 100)
+	printf(" ");
+	}
+	printf("\n");
+}
+
