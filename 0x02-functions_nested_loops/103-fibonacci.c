d6705cb09e59446974262f6fd8604a4a22a6d34b
mmanyuat <mmanyuat201@gmail.com>
2023-08-20 11:20:49 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - program to print fibonnacci less than 40000
+ * Return:always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	long j = i, k = 2, sum = k;
+
+	while (k + j < 4000000)
+	{
+	k += j;
+	if (k % 2 == 0)
+	sum += k;
+	j = k - j;
+	++i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
