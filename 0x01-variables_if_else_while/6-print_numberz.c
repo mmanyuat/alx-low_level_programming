fc8d4d69fde6015ed6d61d1ab1df5315188edccd
mmanyuat <mmanyuat201@gmail.com>
2023-08-26 06:22:08 -0700

    printing numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print digits from 0-9
+ * Return:always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48 ; i <= 57; i++)
+	{
+	putchar (i);
+	}
+	putchar('\n');
+	return (0);
+}
