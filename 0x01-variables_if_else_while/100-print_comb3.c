3b6afd3c09e3211beb4619d7373665fee3047fca
mmanyuat <mmanyuat201@gmail.com>
2023-08-26 08:28:41 -0700

    printing tens and ones

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - program to print two digit numbers
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int ones, tens;
+
+	for (tens = 0; tens <= 8; tens++)
+	{
+	for (ones = tens + 1; ones <= 9; ones++)
+	{
+	putchar(tens + '0');
+	putchar(ones + '0');
+	if (tens != 8 || ones != 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
