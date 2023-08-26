6259025362af9c842fbedc22135c928a7cf56601
mmanyuat <mmanyuat201@gmail.com>
2023-08-26 06:05:56 -0700

    printing first ten digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function to print digits
+ * Return:always 0
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+	printf("%d", a);
+	a++;
+	}
+	printf("\n");
+	return (0);
+}
