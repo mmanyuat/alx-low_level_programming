4c0d3b5130f1f3f67904d63a4258bb0da7db7b60
mmanyuat <mmanyuat201@gmail.com>
2023-08-26 04:53:47 -0700

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program to print lowercase aplhabets
+ * Return: always
+ */
+
+int main (void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
