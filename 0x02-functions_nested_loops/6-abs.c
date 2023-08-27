afc0966f372c6787d4ec2590c44d2a8c30ec658d
mmanyuat <mmanyuat201@gmail.com>
2023-08-27 05:31:21 -0700

    evaluating the absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _abs - function to find the absolute value of an integer
+ * @r: the number
+ * Return: the absolute value
+ */
+
+int _abs(int r)
+{
+	if (r < 0)
+	{
+	return (-r);
+	}
+	return (r);
+}
