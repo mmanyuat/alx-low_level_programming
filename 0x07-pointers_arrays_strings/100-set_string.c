ffe9897b6848b7f1624482e88a6527b069ca9099
mmanyuat <mmanyuat201@gmail.com>
2023-08-28 15:10:43 -0700

    string to pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - function to set string to pointer
+ * @s:the string
+ * @to:the pointer
+ * Return:always 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
