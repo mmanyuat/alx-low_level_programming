0b49194949b6c0177a58865ec17965fe37a720eb
mmanyuat <mmanyuat201@gmail.com>
2023-08-30 04:58:35 -0700

    printing number raised to another

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - function to print power of a base to exp
+ * @x:the base number
+ * @y:the exponent
+ * Return: 0 , result
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+	return (-1);
+	}
+	else
+	return (x * _pow_recursion(x, y - 1));
+}
