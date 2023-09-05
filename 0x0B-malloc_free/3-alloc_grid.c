8e0765878f6db9f6a5cabe7ee7f1f2b38ce01cea
mmanyuat <mmanyuat201@gmail.com>
2023-09-05 02:07:53 -0700

    reserving memory for an array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,13 +15,11 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 	{
-	free(ptr);
 	return (NULL);
 	}
 	ptr = malloc(height * sizeof(int *));
 	if (ptr == NULL)
 	{
-	free(ptr);
 	return (NULL);
 	}
 	for (i = 0; i < height; i++)
