9199d0907a026abb9a75b783d0a1c38cab97f930
mmanyuat <mmanyuat201@gmail.com>
2023-09-06 03:07:26 -0700

    printing all arguemnts

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - function to print all its arguemnts
+ * @argc:the argument counter
+ * @argv:the arguemnt vector
+ * Return:Nothing
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+	printf("\n");
+	return (0);
+}
