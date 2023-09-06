c87be6b5f7c13af2c08ddce014a2058b1e1c5a10
mmanyuat <mmanyuat201@gmail.com>
2023-09-06 05:38:34 -0700

    multiplying nuumbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function to print sum of numbers
+ * @argc:the counter
+ * @argv:the vector
+ * Return:nothing
+ */
+
+int main(int argc, char *argv[])
+{
+	int
+}
