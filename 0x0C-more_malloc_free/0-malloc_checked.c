9eeb45bae7dfb4a4d6d5d238fa4f885499f4d9fd
mmanyuat <mmanyuat201@gmail.com>
2023-09-07 02:31:08 -0700

    memory allocation

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -12,7 +11,7 @@ void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
-	ptr = malloc(sizeof(int) * b);
+	ptr = malloc(b);
 
 	if (ptr == NULL)
 	{
