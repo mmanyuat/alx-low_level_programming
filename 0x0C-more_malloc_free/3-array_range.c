d3754da71a8ffb94ec4821eb3a9bd0fde030127a
mmanyuat <mmanyuat201@gmail.com>
2023-09-08 01:40:09 -0700

    Printing arrays of integrs

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,4 +9,23 @@
  * Return:the pointer
  */
 
-int *array_range(int min, int max);
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i;
+
+	if (min > max)
+	{
+	return (NULL);
+	}
+	ptr = malloc((max - min + 1) * sizeof(int));
+	if (ptr == NULL)
+	{
+	return (NULL);
+	}
+	for (i = min ; i <= max; i++)
+	{
+	ptr[i - min] = i;
+	}
+	return (ptr);
+}
