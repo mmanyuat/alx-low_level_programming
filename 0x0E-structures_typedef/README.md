c4afc359ee43f556982b2b2d4428ba5656923353
mmanyuat <mmanyuat201@gmail.com>
2023-09-11 04:25:35 -0700

    Dog details

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+README file for structures projects
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structures about a dog
+ * @name:the dog`s name
+ * @age:its age
+ * @owner:its owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
