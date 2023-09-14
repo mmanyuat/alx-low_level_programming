eb0af98bc8a55e46acd4e1af6e7fbc15e71e50cc
mmanyuat <mmanyuat201@gmail.com>
2023-09-14 10:47:07 -0700

    printng strings using variadic func

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - functin to print strings
+ * @separator:the comma character that separates the string
+ * @n:number of string passed to the function
+ * Return:nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *sen;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+	sen = va_arg(args, char *);
+	if (sen  == NULL)
+	{
+	printf("(nil)");
+	}
+	printf("%s", sen);
+	if (i < n - 1 && separator != NULL)
+	{
+	printf("%s", separator);
+	}
+	}
+	va_end(args);
+	printf("\n");
+}
