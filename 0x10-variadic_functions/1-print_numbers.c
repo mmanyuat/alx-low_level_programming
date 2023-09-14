f8575a1c46329499be19438c2651d5c4245cd993
mmanyuat <mmanyuat201@gmail.com>
2023-09-14 07:26:01 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,11 +14,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list list;
 
-
-	if (separator == NULL)
-	{
-	return;
-	}
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
