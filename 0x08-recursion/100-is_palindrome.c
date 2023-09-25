737323f4b2877f661669704b70fdeff7fecf72be
mmanyuat <mmanyuat201@gmail.com>
2023-09-25 01:41:59 -0700

    all the files

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * is_palindrome_helper - function to help
+ * @s:the char
+ * @start:the start
+ * @end: the end
+ * Return: 0 or -1
+ */
+
+int is_palindrome_helper(char *s, int start, int end)
+{
+	if (start >= end)
+	return (1);
+	if (s[start] != s[end])
+	return (0);
+	return (is_palindrome_helper(s, start + 1, end - 1));
+}
+
+/**
  * is_palindrome - function to check whether its palindrome
  * @s:the char
  * Return:s or -1
@@ -9,18 +26,8 @@
 int is_palindrome(char *s)
 {
 	int len = 0;
-	int i;
 
 	while (s[len] != '\0')
 	len++;
-	if (len == 0 || len == 1)
-	return (-1);
-	if (s[0] != s[len - 1])
-	return (0);
-	for (i = 1; i < len - 1; i++)
-	{
-	if (s[i] != s[len - 1 - i])
-	return (0);
-	}
-	return (1);
+	return (is_palindrome_helper(s, 0, len - 1));
 }
