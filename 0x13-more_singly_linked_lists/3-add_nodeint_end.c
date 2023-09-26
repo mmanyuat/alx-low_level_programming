23117c32715ff5d2f82486c38a8448bfa9d84447
mmanyuat <mmanyuat201@gmail.com>
2023-09-26 11:34:13 -0700

    finally

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,7 +11,7 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
-	listint_t temp;
+	listint_t *temp;
 
 	(void)temp;
 
@@ -32,6 +32,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	temp = temp->next;
 	}
 	temp->next = new;
-	}
 	return (new);
 }
