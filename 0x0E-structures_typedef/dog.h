32c7390f760411184dcea7fd5a545ba5aec46acb
mmanyuat <mmanyuat201@gmail.com>
2023-11-06 09:41:12 -0800

    defining a struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - name of the struct
+ * @name:member of the name
+ * @age:the age of the dog
+ * @owner:name of the dog`s owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
