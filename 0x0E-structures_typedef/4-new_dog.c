39138ee8ba935268b2a1fc52fdc41c563bdd1f8b
mmanyuat <mmanyuat201@gmail.com>
2023-11-06 10:49:02 -0800

    all

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,9 @@
+#include "dog.h"
+
+/**
+ * new_dog - function that creates new dog
+ * @name:name of the dog
+ * @age:the age of the dog
+ * @owner:the owner of the dog
+ *
+dog_t *new_dog(char *name, float age, char *owner)
