d7d107170dc1daa81a26665a056da192117be3b4
mmanyuat <mmanyuat201@gmail.com>
2023-11-06 10:26:51 -0800

    function to intilize

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - function that prints dog`s struct
+ * @d:the pointer to the struct
+ * Return:nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+	printf("Age: %.6f\n", d->age);
+	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+}
