932bd6fbfada34d447c5a3d9a1cb2e94dc699e68
mmanyuat <mmanyuat201@gmail.com>
2023-11-08 09:22:26 -0800

    pointer to iterate

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,5 +10,12 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	action = (array, size);
+	size_t i;
+
+	if (array == NULL || *action == NULL)
+	return;
+	for (i = 0; i < size; i++)
+	{
+	action(array[i]);
+	}
 }
