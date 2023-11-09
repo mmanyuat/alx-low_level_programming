8b87bf9aafb64c61f21a6079e551efaf42dac00c
mmanyuat <mmanyuat201@gmail.com>
2023-11-09 10:47:12 -0800

    printing list of numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - function to print numbers passed
+ * @separator: the operator after number
+ * @n:the number of parameters
+ * Return:nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int num;
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+	num = va_arg(list, int);
+	printf("%d", num);
+	{
+	if (i < n - 1)
+	if (separator != NULL)
+	printf("%s ", (separator));
+	}
+	}
+	printf("\n");
+	va_end(list);
+}
