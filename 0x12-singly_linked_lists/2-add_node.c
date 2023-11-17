a5e641c30b923a088c76b91f65a2cadb8e2f15bd
mmanyuat <mmanyuat201@gmail.com>
2023-11-16 21:14:52 -0800

    all

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,7 +16,7 @@ list_t *add_node(list_t **head, const char *str)
 	new_node = malloc(sizeof(list_t));
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
-	if (new_node->str == NULL)
+	if (new_node == NULL)
 	return (NULL);
 	new_node->next = (*head);
 	(*head) = new_node;
