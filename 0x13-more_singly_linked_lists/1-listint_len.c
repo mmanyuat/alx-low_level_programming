bc850eb1c1d97be5cd5553500fdf2f06f2aeb940
mmanyuat <mmanyuat201@gmail.com>
2023-11-22 09:02:11 -0800

    no of nodes

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - function to print length of a node
+ * @h:the pointer to the node header
+ * Return:the number of the nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	while(h != NULL)
+	{
+	count++;
+	h = h->next;
+	}
+	return (count);
+}
