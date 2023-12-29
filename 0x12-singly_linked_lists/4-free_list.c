f9c5564ad9c81fd2bd78bcf113d43d69ab119004
mmanyuat <mmanyuat201@gmail.com>
2023-12-29 00:30:04 -0800

    updated

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,8 +9,19 @@
 
 void free_list(list_t *head)
 {
+	list_t *next;
+	list_t *current;
+
 	if (head == NULL)
 	{
-	free(head);;
+	return;
+	}
+	current = head;
+	while (current != NULL)
+	{
+	next = current->next;
+	free(current->str);
+	free(current);
+	current = next;
 	}
 }
