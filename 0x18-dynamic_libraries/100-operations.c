09ccfce6e603634a57d4a8c0d2e47442094cba49
mmanyuat <mmanyuat201@gmail.com>
2024-01-15 11:37:14 -0800

    python and c

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - function to add two numbers
+ * @a:the first number
+ * @b:the second number
+ * Return:the sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - function that subtracts two numbers
+ * @a:the first number
+ * @b:the second number
+ * Return:the difference
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function multiplies two numbers
+ * @a:first number
+ * @b:the second number
+ * Return: the product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - function to divide two numbers
+ * @a: the first number
+ * @b:the second number
+ * Return:the quotient
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - function to find the modolo
+ * @a:the fist
+ * @b: the second
+ * Return:the modulos
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
